ab0533daa96ebc0390801a3f147d31ee5e3d0dab
hobbyprogr4mmer <194447875+hobbyprogr4mmer@users.noreply.github.com>
2025-03-08 02:03:59 +1100

    Added more help and error messages, fixed functionalities, should be good to use

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# passgen
+# Password Generator
+# Generate multiple passwords at a time
+# Can be used to generate any random string comprising of a set of characters (numbers, uppercase, special ,mixed)
+# Help can be found with: passgen h
+# Prototype for future projects to come
--- passgen.c
+++ passgen.c
@@ -18,22 +18,34 @@ int genPass(int, int, Passtype);
 
 int main(int argc, char *argv[]){
     Passtype type;
-    // initalize pseudorandom seed based on current time
-    srand(time(0));
+    // initalize pseudorandom seed based on current time with added variance (clock)
+    srand(time(0) + clock());
+    argHandler(argc, argv, type);
 
     return 0;
 }
 
 int argHandler(int argc, char *argv[], Passtype type){
     if(argc > 4){
-        printf("Too many arguments");
+        printf("Too many arguments, try using h for help\n");
         return 1;
     
     // this handles flags passed into exec
-    } else if(argc == 3){
+    } else if(argc == 2) {
+        switch(*argv[1]){
+            case 'h':
+            printf("Usage:\tpassgen [type (u, l, n, m, s)] [character count integer] [password count integer]\n\t type flags are: upper, lower, number, mixed, special\n");
+            break;
+
+            default:
+            printf("Error: did not use type flags and integers to specify char and pass counts use [passgen h] for help\n");
+            return 1;
+        }
+    }
+    else if(argc == 4){
         // Must convert character array (strings to integers)
-        /*int charcount = argv[2];
-        int passcount = argv[3];*/
+        int passcount = atoi(argv[2]);
+        int charcount = atoi(argv[3]);
         switch(*argv[1]){
             case 'u':
                 printf("Uppercase selected\n");
@@ -54,7 +66,6 @@ int argHandler(int argc, char *argv[], Passtype type){
                 printf("Mixed selected\n");
                 type = MIXED;
                 break;
-                genPass(passcount, charcount, type);
 
             case 's':
                 printf("Special selected\n");
@@ -63,48 +74,73 @@ int argHandler(int argc, char *argv[], Passtype type){
 
             default:
                 printf("Error: not a flag, try [passgen h] for help\n");
+                return 1;
             }
+        genPass(passcount, charcount, type);
     } else {
-        printf("Error: did not use integers to specify char and pass counts\n");
-        return 0;
-    }  
+        printf("Error: did not use type flags and integers to specify char and pass counts use [passgen h] for help\n");
+        return 1;
+    }
+    return 0;
 }
 
 
 int genPass(int passcount, int charcount, Passtype type){
     // constant for ascii table vals
     const int RANDMAX = 128;
-    int r = rand();
     while(passcount > 0){
         char pass[(charcount*sizeof(int))+1];
         for(int i = 0; i < charcount; i++){
+            int r = rand();
             // match types with respective logic
             if(type == MIXED){
                 // checks if rand is out of ascii scope and randomizes until it isnt
-                while(128 == (r % RANDMAX) && (r % RANDMAX) < 33){
+                while(128 == (r % RANDMAX) || (r % RANDMAX) < 33){
                     r = rand();
                 }
                 // passes value when rand is within scope
                 if(127 > (r % RANDMAX) && (r % RANDMAX) >= 33){
-                    pass[i] = r;
+                    pass[i] = r % RANDMAX;
+                    r = rand();
                 }
             } else if(type == UPPER){
                 // checks if rand is out of ascii upper char scope
-                while(90 < (r % RANDMAX) && (r % RANDMAX) < 65){
+                while(90 < (r % RANDMAX) || (r % RANDMAX) < 65){
                     r = rand();
                 }
                 // pass value
                 if(90 >= (r % RANDMAX) && (r % RANDMAX) >= 65){
-                    pass[i] = r;
+                    pass[i] = r % RANDMAX;
                 }
             } else if(type == LOWER){
-                //Fill logic here, 2 more to go!
+                while(122 < (r % RANDMAX) || (r % RANDMAX) < 97){
+                    r = rand();
+                } 
+                if(122 >= (r % RANDMAX) && (r % RANDMAX) >= 97){
+                    pass[i] = r % RANDMAX;
+                }
+            } else if(type == NUMBER){
+                while(57 < (r % RANDMAX) || (r % RANDMAX) < 48){
+                    r = rand();
+                }
+                if(57 >= (r % RANDMAX) && (r % RANDMAX) >= 48){
+                    pass[i] = r % RANDMAX;
+                }
+            } else if(type == SPECIAL){
+                char specialChars[] = "!@#$%^&*()-_=+[{]};:'\",<.>/?";
+                int specialCharCount = strlen(specialChars);
+
+                r = rand() % specialCharCount;
+                pass[i] = specialChars[r];
+                } else {
+                printf("Aborted due to error\n");
+                return 1;
             }
             printf("%c", pass[i]); 
         }
-        // seperate passwords
+         // seperate passwords
         printf("\n");
-        passcount--;
+        passcount--;   
     }
     return 0;
 }
